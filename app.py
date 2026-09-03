89fb7690363d6cf20356ef286350423971fb1696
Vini Jacob <vinijacob@MacBook-Air-de-Vini.local>
2026-09-03 18:19:28 -0300

    Adiciona import do redirect do Flask

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, session
+from flask import Flask, render_template, request, jsonify, session, redirect
 import sqlite3
 import requests
 import uuid
